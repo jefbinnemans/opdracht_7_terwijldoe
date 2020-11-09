f16db88a969842781afc6422767e286171777e5e
jefbinnemans <41265139+jefbinnemans@users.noreply.github.com>
2020-11-09 15:13:58 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let index = 9
+while (index != 0) {
+    basic.showNumber(index)
+    index += -1
+    basic.pause(600)
+}
+while (true) {
+    basic.showIcon(IconNames.Butterfly)
+    basic.showIcon(IconNames.Tortoise)
+    basic.clearScreen()
+}
